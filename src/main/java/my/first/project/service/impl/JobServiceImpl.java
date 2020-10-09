67474d7b2595e699bbabbd9a35e5f270c09cadce
nguyen_tran_kyo <tranducnguyenk37@gmail.com>
2020-10-09 20:57:08 +0700

    Cuối buổi

---
+++ src/main/java/my/first/project/service/impl/JobServiceImpl.java
@@ -0,0 +1,44 @@
+package my.first.project.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import my.first.project.model.Job;
+import my.first.project.model.User;
+import my.first.project.service.IJobService;
+
+@Service
+public class JobServiceImpl implements IJobService{
+
+	@Override
+	public void insertJob(Job job) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateJob(Job job) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteJob(Job job) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Job> getJobsByUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void assignRoleByUser() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
